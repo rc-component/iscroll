07323e659ec2b6f3304800df25deecaad9073089
chemzqm <chemzqm@gmail.com>
2017-07-23 18:31:44 +0800

    fix prepublish script

--- .scripts/prepublish.sh
+++ .scripts/prepublish.sh
@@ -12,5 +12,3 @@ rm -rf ./dist
 ./node_modules/.bin/babel --ignore tests,stories --plugins "transform-runtime" ./src --out-dir ./dist
 echo ""
 echo "=> Transpiling completed."
-
-. .scripts/user/prepublish.sh
--- .scripts/user/prepublish.sh
+++ .scripts/user/prepublish.sh
@@ -1,2 +1,1 @@
 # Use this file to your own code to run at NPM `prepublish` event.
-postcss --use postcss-cssnext -d dist src/*.css
